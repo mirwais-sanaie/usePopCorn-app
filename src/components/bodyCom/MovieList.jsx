0456ff761029fdc8d33e090d6dd6acb38077cbb8
Mirwais <mirwais.sanaie@gmail.com>
2024-12-01 21:07:24 +0430

    Movie-list ui added.

--- src/components/bodyCom/MovieList.jsx
+++ src/components/bodyCom/MovieList.jsx
@@ -1,11 +1,17 @@
 function MovieList() {
   return (
     <div className="box lg:w-[26rem] h-full">
-      <ul>
-        <li>Hello world</li>
-        <li>Hello world</li>
-        <li>Hello world</li>
-        <li>Hello world</li>
+      <button className="toggle-show">&#8722;</button>
+      <ul className="list-movie">
+        <li>
+          <img src="./test.jpg" className="movie-img-list" alt="" />
+          <div className="flex flex-col text-start ps-5 space-y-2">
+            <h3 className=" text-lg font-bold">Title of movie</h3>
+            <p>
+              <span>🗓</span> 2016
+            </p>
+          </div>
+        </li>
       </ul>
     </div>
   );
--- src/components/bodyCom/WatchList.jsx
+++ src/components/bodyCom/WatchList.jsx
@@ -1,6 +1,8 @@
 function WatchList() {
   return (
     <div className="box lg:w-[26rem] h-full">
+      <button className="toggle-show">&#8722;</button>
+
       <ul>
         <li>Hello world</li>
         <li>Hello world</li>

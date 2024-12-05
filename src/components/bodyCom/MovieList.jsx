c32a16b24b56b2905608788b82f4ec18b11f920a
Mirwais <mirwais.sanaie@gmail.com>
2024-12-05 21:36:40 +0430

    show and hide panel added.

--- src/components/bodyCom/MovieList.jsx
+++ src/components/bodyCom/MovieList.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
+
 function MovieList() {
+  const [showPanel, setShowPanel] = useState(true);
+
   return (
     <div className="box lg:w-[26rem] h-full">
-      <button className="toggle-show">&#8722;</button>
-      <ul className="list-movie hover-color">
-        <li>
-          <img src="./test.jpg" className="movie-img-list" alt="" />
-          <div className="flex flex-col text-start ps-5 space-y-2">
-            <h3 className=" text-lg font-bold">Title of movie</h3>
-            <p>
-              <span>🗓</span> 2016
-            </p>
-          </div>
-        </li>
-      </ul>
+      <button
+        onClick={() => setShowPanel((showPanel) => !showPanel)}
+        className="toggle-show"
+      >
+        {showPanel ? <span>&#8722;</span> : <span>&#43;</span>}
+      </button>
+      {showPanel && (
+        <ul className="list-movie">
+          <li>
+            <img src="./test.jpg" className="movie-img-list" alt="" />
+            <div className="flex flex-col text-start ps-5 space-y-2">
+              <h3 className=" text-lg font-bold">Title of movie</h3>
+              <p>
+                <span>🗓</span> 2016
+              </p>
+            </div>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/bodyCom/WatchList.jsx
+++ src/components/bodyCom/WatchList.jsx
@@ -1,55 +1,70 @@
+import { useState } from "react";
+
 function WatchList() {
+  const [showPanel, setShowPanel] = useState(true);
+
   return (
     <div className="box lg:w-[26rem] h-full">
-      <button className="toggle-show">&#8722;</button>
+      <button
+        onClick={() => setShowPanel((showPanel) => !showPanel)}
+        className="toggle-show"
+      >
+        {showPanel ? <span>&#8722;</span> : <span>&#43;</span>}
+      </button>
 
-      <div className="summary shadow-xl">
-        <h3 className="uppercase font-bold mb-4">Movie You watched</h3>
-        <div className="flex space-x-5">
-          <p>
-            <span>#️⃣</span>
-            <span>2 movies</span>
-          </p>
-          <p>
-            <span>⭐️</span>
-            <span>8.50</span>
-          </p>
-          <p>
-            <span>🌟</span>
-            <span>8.50</span>
-          </p>
-          <p>
-            <span>⏳</span>
-            <span>23 min</span>
-          </p>
-        </div>
-      </div>
-
-      <ul className="list-movie">
-        <li className="relative">
-          <img src="./test.jpg" className="movie-img-list" alt="" />
-          <div className="flex flex-col text-start ps-5 space-y-2">
-            <h3 className=" text-lg font-bold">Title of movie</h3>
-            <div className="flex justify-between">
-              <div className="flex space-x-5 ">
-                <p>
-                  <span>⭐️</span>
-                  <span>8.50</span>
-                </p>
-                <p>
-                  <span>🌟</span>
-                  <span>8.50</span>
-                </p>
-                <p>
-                  <span>⏳</span>
-                  <span>23 min</span>
-                </p>
-              </div>
-              <button class="btn-delete absolute right-14">X</button>
+      {showPanel && (
+        <>
+          {" "}
+          <div className="summary shadow-xl">
+            <h3 className="uppercase font-bold mb-4">Movie You watched</h3>
+            <div className="flex lg:space-x-5 space-x-3 text-sm lg:text-base">
+              <p>
+                <span>#️⃣</span>
+                <span>2 movies</span>
+              </p>
+              <p>
+                <span>⭐️</span>
+                <span>8.50</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>8.50</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>23 min</span>
+              </p>
             </div>
           </div>
-        </li>
-      </ul>
+          <ul className="list-movie">
+            <li className="relative">
+              <img src="./test.jpg" className="movie-img-list" alt="" />
+              <div className="flex flex-col text-start ps-5 space-y-2">
+                <h3 className=" text-lg font-bold">Title of movie</h3>
+                <div className="flex justify-between">
+                  <div className="flex space-x-5 ">
+                    <p>
+                      <span>⭐️</span>
+                      <span>8.50</span>
+                    </p>
+                    <p>
+                      <span>🌟</span>
+                      <span>8.50</span>
+                    </p>
+                    <p>
+                      <span>⏳</span>
+                      <span>23 min</span>
+                    </p>
+                  </div>
+                  <button class="btn-delete absolute lg:right-14 right-5">
+                    X
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 }

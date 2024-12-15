fc426145aedf0cbcf8b54b8e2b835f5f2451ce77
Mirwais <mirwais.sanaie@gmail.com>
2024-12-15 23:52:08 +0430

    opened movie styles added .

--- src/components/bodyCom/OpenedMovie.jsx
+++ src/components/bodyCom/OpenedMovie.jsx
@@ -1,10 +1,22 @@
 function OpenedMovie({ setSelectedMovie, selectedMovie }) {
   return (
-    <div>
-      <button onClick={() => setSelectedMovie(null)} className="back-icon">
+    <div className="relative">
+      <button
+        onClick={() => setSelectedMovie(null)}
+        className="back-icon absolute left-2 top-2"
+      >
         <span>&#8592;</span>
       </button>
-      <h1>Hello world ID : {selectedMovie}</h1>
+      {/* <h1>Hello world ID : {selectedMovie}</h1> */}
+      <div className="movie-s-content">
+        <img className="opened-img" src="./test.jpg" alt="" />
+        <div className="detail-opened">
+          <h1 className="font-bold text-2xl mb-7">prison break</h1>
+          <p className="mb-2 text-sm">25 Jun 2015 • 119 min</p>
+          <p className="mb-2 text-sm">Documentary</p>
+          <p className="mb-2 text-sm">⭐️ 8.0 IMDb rating</p>
+        </div>
+      </div>
     </div>
   );
 }

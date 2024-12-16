d746c16bdf4dd29268965dd213f3a091dd7219e0
Mirwais <mirwais.sanaie@gmail.com>
2024-12-16 21:31:48 +0430

    some style added.

--- src/components/bodyCom/OpenedMovie.jsx
+++ src/components/bodyCom/OpenedMovie.jsx
@@ -10,11 +10,13 @@ function OpenedMovie({ setSelectedMovie, selectedMovie }) {
       {/* <h1>Hello world ID : {selectedMovie}</h1> */}
       <div className="movie-s-content">
         <img className="opened-img" src="./test.jpg" alt="" />
-        <div className="detail-opened">
-          <h1 className="font-bold text-2xl mb-7">prison break</h1>
-          <p className="mb-2 text-sm">25 Jun 2015 • 119 min</p>
-          <p className="mb-2 text-sm">Documentary</p>
-          <p className="mb-2 text-sm">⭐️ 8.0 IMDb rating</p>
+        <div className="detail-opened py-3">
+          <h1 className="font-bold text-xl mb-7">
+            Going Clear: Scientology & the Prison of Belief
+          </h1>
+          <p className="mb-3 text-sm">25 Jun 2015 • 119 min</p>
+          <p className="mb-3 text-sm">Documentary</p>
+          <p className="mb-3 text-sm">⭐️ 8.0 IMDb rating</p>
         </div>
       </div>
     </div>

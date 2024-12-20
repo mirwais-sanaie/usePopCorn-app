20b7a672029c025ea8501aadc32fef5a1d97e93c
Mirwais <mirwais.sanaie@gmail.com>
2024-12-21 01:39:40 +0430

    hovering on stars added.

--- src/components/bodyCom/StarRating.jsx
+++ src/components/bodyCom/StarRating.jsx
@@ -1,20 +1,26 @@
-import { hover } from "@testing-library/user-event/dist/hover";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 function StarRating({ size = 10 }) {
   const [hovering, setHovering] = useState(0);
+  const [onMouseOver, setOnMouseOver] = useState(false);
 
   return (
-    <div className="flex gap-x-4">
+    <div
+      onMouseLeave={() => setOnMouseOver((onMouseOver) => !onMouseOver)}
+      onMouseEnter={() => setOnMouseOver((onMouseOver) => !onMouseOver)}
+      className="flex gap-x-4"
+    >
       <span className="flex justify-center gap-x-1">
         {Array.from({ length: size }, (_, i) => {
           return (
             <FaStar
-              onMouseOver={() => setHovering((hovering) => (hovering = i + 1))}
+              onMouseOver={() => {
+                setHovering((hovering) => (hovering = i + 1));
+              }}
               key={i}
               size={21}
-              fill="none"
+              fill={onMouseOver && i <= hovering - 1 ? "#fcc419" : "none"}
               style={{
                 stroke: "#fcc419", // Border color
                 strokeWidth: "30", // Border thickness
@@ -24,7 +30,7 @@ function StarRating({ size = 10 }) {
         })}
       </span>
       <span className="font-semibold text-[17px] text-[#fcc419]">
-        {hovering}
+        {onMouseOver ? hovering : null}
       </span>
     </div>
   );
